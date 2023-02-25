2f1767930b7d9374df3394111e8fcfeb98936c03
Adithya Jayan <adithyaj8@gmail.com>
2023-02-25 12:10:55 +0530

    Added sorting option in gallery , also removed vertical scrollbar from html.

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@ async function LoadGallery(){
           var photoContainer = document.createElement("a");
           var photo = document.createElement("img");
 
-          photoContainer.id = "DynamicGallery";
           photoContainer.className = "photolink";
           photo.className = "photo";
 
@@ -16,16 +15,23 @@ async function LoadGallery(){
             let content = "";
             let headers = photoData[0].split(",");
             for (var i = 1; i < photoData.length; i++) {
-              var photoinfo = photoData[i].split(",");
+              var photoinfo = photoData[i].replace('\r','').split(",");
               var newPhotoContainer = photoContainer.cloneNode(true);
               var newPhoto = photo.cloneNode(true);
             
               //Set source of inner image and alt value first
               newPhoto.setAttribute(headers[0], photoinfo[0]); //Append attribute
               newPhoto.setAttribute(headers[1], photoinfo[1]); //Append attribute
+              
+              //Append tags to the photo class name
+              let final_attr = photoinfo[headers.length - 1];
+              if( newPhoto.getAttribute(headers[headers.length - 1]) != null) {
+                final_attr =  newPhoto.getAttribute(headers[headers.length - 1])+" "+photoinfo[headers.length - 1];
+              }
+              newPhoto.setAttribute(headers[headers.length - 1],final_attr); 
 
               //Then set main image data
-              for(var j = 2; j < headers.length; j++) {
+              for(var j = 2; j < headers.length - 1; j++) {
                 if(photoinfo[j] != ""){
                   let final_attr = photoinfo[j];
                   if( newPhotoContainer.getAttribute(headers[j]) != null) {
@@ -63,10 +69,6 @@ async function LoadGallery(){
     
 };
 
-async function Runinsequence(){
-  await LoadGallery();
-}
-
 function waitForElement(elementPath, callBack){
   window.setTimeout(function(){
     if($(elementPath).length){
@@ -76,13 +78,10 @@ function waitForElement(elementPath, callBack){
     }
   },500)
 }
+async function Runinsequence(){
+  await LoadGallery();
 
-$(document).ready(function() {
-  
-  console.log("Starting Load Gallery");
-  Runinsequence();
-
-  waitForElement("#DynamicGallery",function(){
+  waitForElement("#gallery",function(){
     //Initialize isotope gallery
     console.log("Starting Isotope");
     $('.grid').isotope({
@@ -91,15 +90,44 @@ $(document).ready(function() {
       percentPosition: true,
       layoutMode: 'masonry'
     });
+
+    // bind filter button click
+    $('.filters-button-group').on( 'click', 'button', function() {
+      var filterValue = $( this ).attr('data-filter');
+      // use filterFn if matches value
+      $('.grid').isotope({ filter: filterValue });
+    });
+
+    // change is-checked class on buttons
+    $('.button-gal-group').each( function( i, buttonGroup ) {
+      var $buttonGroup = $( buttonGroup );
+      $buttonGroup.on( 'click', 'button', function() {
+        $buttonGroup.find('.is-checked').removeClass('is-checked');
+        $( this ).addClass('is-checked');
+      });
+    });
     console.log("Isotope initialized");
 
     console.log("Starting magnific");
     $('.portfolio .grid .photolink').magnificPopup({
       type: 'image',
       // other options
-      gallery:{enabled:true}
+      gallery:{enabled:true},
+      image: {
+        // options for image content type
+        titleSrc: function(item) {
+            return item.el.attr('data-description') + '<small>' + item.el.attr('subtitle') + '</small>';
+          }
+      }
     });
     console.log("Loaded magnific");
-});
+  });
+
+}
+
+$(document).ready(function() {
+  
+  console.log("Starting Load Gallery");
+  Runinsequence();
   
 });
